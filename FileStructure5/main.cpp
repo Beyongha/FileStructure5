8c13c17b8da7e2d22c291d27e028325f4229e441
Beyongha <ahffk0876@gmail.com>
2016-06-06 18:45:13 +0900

    Initial Commit

---
+++ FileStructure5/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  FileStructure5
+//
+//  Created by JIN Beyongha on 2016. 6. 6..
+//  Copyright © 2016년 JIN Beyongha. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
