4337a6874344302aec8f2b1e4d73b8052b39e6a5
Beyongha <ahffk0876@gmail.com>
2016-06-08 02:30:12 +0900

    insert, append 해결하면됨.

---
+++ FileStructure5/RecordFile.h
@@ -0,0 +1,24 @@
+//
+//  RecordFile.h
+//  FileStructure5
+//
+//  Created by 진병하 on 2016. 6. 7..
+//  Copyright © 2016년 JIN Beyongha. All rights reserved.
+//
+
+#ifndef RecordFile_h
+#define RecordFile_h
+
+#include "Buffer.h"
+#include "BufferFile.h"
+
+#include <iostream>
+
+using namespace std;
+
+template <class Record>
+class RecordFile : BufferFile {
+    
+};
+
+#endif /* RecordFile_h */
